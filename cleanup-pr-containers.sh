a200cc1aacdf52776264a123c9802262d8cbf252
Jordan Cotter <jrdn.cttr@gmail.com>
2017-07-27 12:45:30 -0500

    fixed cleanup script and added new script for manual updates

--- cleanup-pr-containers.sh
+++ cleanup-pr-containers.sh
@@ -1,4 +1,6 @@
-containerId=$(docker ps -q | tail -n 1)
+containerId=$(docker ps -q | xargs docker inspect --format="{{.Name}}" $1 | grep -v "hoegg-site" | tail -n 1)
 
-docker stop $containerId
-docker rm $containerId
+if [[ $containerId != "" ]]; then
+	docker stop $containerId
+	docker rm $containerId
+fi
--- testpr.sh
+++ testpr.sh
@@ -9,12 +9,13 @@ else
 	let port=$(docker ps -q | xargs docker inspect --format="{{.Name}}" $1 | grep -v "hoegg-site" | rev | cut -c-4 | rev | xargs echo $1 | cut -c-4)+1
 fi
 
-$(/root/.local/bin/aws ecr get-login) 
-trigger=$(docker pull $dockerImage | tail -n 1)
-#echo $(TZ=America/Chicago date) $trigger $port >> /root/debug.log
-if [[ $trigger == *"Downloaded newer image"* ]]; then  
-	docker run -p $port:3000 --name hoegg-pr$port -d $dockerImage	
-	bash /root/scripts/slackbot.sh "#web-site" "new pr. View at http://162.243.54.40:$port"
-	#bash /root/scripts/testslackbot.sh "#web-site" "new pr. View at http://162.243.54.40:$port"
-	bash /root/scripts/testRoutes.sh http://162.243.54.40:$port
-fi
+echo $port
+#$(/root/.local/bin/aws ecr get-login) 
+#trigger=$(docker pull $dockerImage | tail -n 1)
+##echo $(TZ=America/Chicago date) $trigger $port >> /root/debug.log
+#if [[ $trigger == *"Downloaded newer image"* ]]; then  
+#	docker run -p $port:3000 --name hoegg-pr$port -d $dockerImage	
+#	bash /root/scripts/slackbot.sh "#web-site" "new pr. View at http://162.243.54.40:$port"
+#	#bash /root/scripts/testslackbot.sh "#web-site" "new pr. View at http://162.243.54.40:$port"
+#	bash /root/scripts/testRoutes.sh http://162.243.54.40:$port
+#fi
